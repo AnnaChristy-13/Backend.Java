cfdb077cab089eb01af03abd430d8ed1d2760308
AnnaChristy-13 <annachristy13@gmail.com>
2024-01-17 17:59:50 +0100

    lesson1 update 2

---
+++ src/lessons/Lesson2ByteIntDouble.java
@@ -0,0 +1,20 @@
+package lessons;
+
+public class Lesson2ByteIntDouble {
+    public static void main(String[] args) {
+
+        int x = 20;
+        int y = 4;
+
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        double xdouble = 20.8;
+        double ydouble = 4.4;
+
+        System.out.println( xdouble / ydouble);
+        System.out.print( xdouble + ydouble) ;
+
+
+    }
+}
