b037a9cb52ff8e4d7bd99ecc7b1b92d2400dd9aa
AnnaChristy-13 <annachristy13@gmail.com>
2024-01-17 17:21:22 +0100

    lesson1 update 2

---
+++ src/lessons/Lesson2.java
@@ -0,0 +1,13 @@
+package lessons;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Hello World");
+    }
+
+}
+
+
+
+
