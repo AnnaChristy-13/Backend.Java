c1f260a8d916b032eb1d7122bbda677472d9a65f
AnnaChristy-13 <annachristy13@gmail.com>
2024-01-18 08:47:28 +0100

    lesson1 update 2

---
+++ src/homework/Homework2Task2.java
@@ -0,0 +1,19 @@
+package homework;
+
+public class Homework2Task2 {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("char:@");
+        System.out.println("byte:127");
+        System.out.println("int:2147483647");
+        System.out.println("short:32766");
+        System.out.println("long:9223372036854775999");
+        System.out.println("float:123.4567890");
+        System.out.println("double:123.456789010111213");
+
+    }
+
+
+
+}
---
+++ src/homework/Homework2Task3.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class Homework2Task3 {
+    public static void main(String[] args) {
+
+        System.out.println("Произвольное трехзначное число 248");
+        System.out.println();
+
+        int x = 8;
+        int y = 6;
+        int z = 2;
+
+        System.out.println(x - y);
+        System.out.println(x / z);
+        System.out.println(y + z);
+    }
+}
